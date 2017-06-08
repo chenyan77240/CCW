f5036866b8c834a557f8bac1d8428d89bc737b98
Candy.Chen <779837984@qq.com>
2017-06-08 21:53:32 +0800

    Commit Project

---
+++ CCWTest.java
@@ -0,0 +1,13 @@
+package com.ccw.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by candy on 2017/6/8.
+ */
+public class CCWTest {
+    @Test
+    public void ccwTest()  {
+        System.out.println("Hello World");
+    }
+}
